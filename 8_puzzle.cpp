7e80bbb5a26eed832d7c9bc8a5c81e781c6d3429
Abhishek Kumar Srivastava <asriv003@ucr.edu>
2016-10-31 05:13:42 -0700

    more modifications

--- 8_puzzle.cpp
+++ 8_puzzle.cpp
@@ -1,11 +1,68 @@
-#include "8_puzzle.h"
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <queue>
 
 using namespace std;
 
-int puzzle[3][3];
+class puzzle
+{
 
+private:
+	int state[3][3];
+	int goal[3][3];
+public:
+	puzzle(int *initial)
+	{
+
+	}
+	~puzzle();
+	void print_node();
+	int get_current_node();
+	void generate_goal_state()
+	{
+		for(int i = 0; i < 3; i++)
+			for(int j = 0; j < 3; j++)
+				goal[i][j] = 3*i+j+1;
+
+		goal[2][2] = 0;
+	}
+};
+
+class Node
+{
+private:
+	int state[3][3];
+	int h;
+	int g;
+public:
+	int priority;
+	Node( int nNewState[][3], int nH, int nG )
+	{
+		for(int i = 0;i < 3; i++)
+			for(int j = 0; j < 3; j++)
+				state[i][j] = nNewState[i][j];
+		h = nH;
+		g = nG;
+		priority = h + g;
+	}
+};
+
+//Overload the > operator.
+bool operator> (const  Node &node1, const Node &node2)
+{
+	return node1.priority < node2.priority;	
+}
+
+
+//The priorities will be assigned in the Descending Order of priority
+priority_queue<Node, vector<Node>,greater<vector<Node>::value_type> > pqNodes;
+
+// Add container elements.
+/*pqStudent2.push( Student( "Mark", 38 ) );
+pqStudent2.push( Student( "Marc", 25 ) );
+*/
 //check valid move
-
 bool is_valid_move()
 {
 
@@ -47,7 +104,7 @@ int misplace_tile()
 
 }
 
-void * misplaced_tile_heuristic()
+void * a_star_misplaced_tile_heuristic()
 {
 
 }
@@ -57,15 +114,13 @@ int manhattan_distance()
 
 }
 
-void * manhattan_distance_heuristic()
+void * a_star_manhattan_distance_heuristic()
 {
 
 }
 
-//queuing function
-
 //general-search
-void general_search(problem puzzle, void *queuing_function())
+void general_search(Node problem, void *queuing_function())
 {
 
 }
@@ -85,15 +140,12 @@ void output(int total_nodes, int max_nodes, int depth)
 //print algorithm choices
 void print_algo_choices()
 {
-		cout<<"Enter your choice of algorithm"<<endl;
-		cout<<"1. Uniform Cost Search"<<endl;
-		cout<<"2. A* with the Misplaced Tile heuristic."<<endl;
-		cout<<"3. A* with the Manhattan distance heuristic."<<endl;
+		
 }
 
 int main(int argc, char const *argv[])
 {
-	int puzzle_choice, algo_choice;
+	int puzzle_choice, algo_choice, start_state[3][3];;
 	
 	cout<<"Welcome to Abhishek's 8-puzzle solver."<<endl;
 	
@@ -103,13 +155,8 @@ int main(int argc, char const *argv[])
 
 	if(puzzle_choice == 1)
 	{
-		//generate random intial state
+		//generate intial state
 
-		//Algorithm Choice Input
-		print_algo_choices();
-		cin>>algo_choice;
-		//general-search
-		//output function
 	} 
 	else if(puzzle_choice == 2)
 	{
@@ -120,21 +167,42 @@ int main(int argc, char const *argv[])
 			cout<<"Enter row: "<<i+1<<"use space or tabs between numbers"<<endl;
 			for(int j = 0; j < 3; j++)
 			{
-				cin>>puzzle[i][j];
+				cin>>start_state[i][j];
 			}
 		}
-
-		//Algorithm Choice Input
-		print_algo_choices();
-		cin>>algo_choice;
-
-		//general-search
-		//output function
 	}
 	else
 	{
 		cout<<"Wrong!!(In trump's Voice)"<<endl;
+		return 0;
 	}
 
+	//Intialization	
+	Node problem(start_state,0,0);
+	//Algorithm Choice Input
+	cout<<"Enter your choice of algorithm"<<endl<<"1. Uniform Cost Search"<<endl<<"2. A* with the Misplaced Tile heuristic."<<endl<<"3. A* with the Manhattan distance heuristic."<<endl;
+	cin>>algo_choice;
+	//general-search
+	if(algo_choice == 1)
+	{
+		cout<<"Using Uniform Cost Search"<<endl;
+		general_search(problem, uniform_cost_search);
+	}
+	else if(algo_choice == 2)
+	{
+		cout<<"Using A* with misplaced tile heuristic"<<endl;
+		general_search(problem,a_star_misplaced_tile_heuristic);
+	}
+	else if(algo_choice == 3)
+	{
+		cout<<"Using A* with manhattan distance heuristic"<<endl;
+		general_search(problem,a_star_manhattan_distance_heuristic);
+	}
+	else
+	{
+		cout<<"Wrong!!(In trump's Voice)"<<endl;
+		return 0;
+	}
+	//output function
 	return 0;
 }
