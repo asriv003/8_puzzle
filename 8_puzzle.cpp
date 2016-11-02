94f1c5bf0419acdf73c13076d7633f1048aded2b
Abhishek Kumar Srivastava <asriv003@ucr.edu>
2016-11-02 11:08:07 -0700

    major changes

--- 8_puzzle.cpp
+++ 8_puzzle.cpp
@@ -8,46 +8,37 @@ using namespace std;
 class Node
 {
 private:
-	int** state = NULL;
+	int state[9];
 	int h_n;
 	int g_n;
 public:
-	int priority;
-	Node();
-	Node( int nNewState[][3], int nH, int nG )
+	int f_n;
+	Node(){}
+	Node( int nNewState[], int nH, int nG )
 	{
-		state = new int*[3];
-		for (int h = 0; h < 3; h++)
-		{
-			state[h] = new int[3];
-			for (int w = 0; w < 3; w++)
-			{
-				state[h][w] = nNewState[h][w];
-			}
-		}
+		for (int i = 0; i < 9; i++)
+			state[i] = nNewState[i];
+
 		h_n = nH;
 		g_n = nG;
-		priority = h_n + g_n;
+		f_n = g_n + h_n;
 	}
-	int ** get_state()
+	int *get_state()
 	{
 		return state;
 	}
-	void set_state(int newState[][3])
+	void set_state(int newState[])
 	{
-		for(int i = 0;i < 3; i++)
-			for(int j = 0; j < 3; j++)
-				state[i][j] = newState[i][j];
+		for(int i = 0;i < 9; i++)
+				state[i] = newState[i];
 	}
 	void print_state()
 	{
-		for(int i = 0; i < 3; i++)
+		for(int i = 0; i < 9; i++)
 		{
-			for(int j = 0; j < 3; j++)
-			{
-				cout<<state[i][j]<<" ";
-			}
-			cout<<endl;
+			cout<<state[i]<<" ";
+			if( (i+1)% 3 == 2)
+				cout<<endl;
 		}
 	}
 };
@@ -57,13 +48,11 @@ class puzzle
 private:
 	Node current_node;
 	Node goal_node;
-	int goal[3][3];
+	int goal[9];
 	queue<Node> visited_nodes;
 public:
-	puzzle(Node n)
-	{
-		current_node = n;
-	}
+	puzzle(){}
+	~puzzle(){}
 	void print_current_node()
 	{
 		current_node.print_state();
@@ -78,12 +67,20 @@ public:
 	}
 	void generate_goal_state()
 	{
-		for(int i = 0; i < 3; i++)
-			for(int j = 0; j < 3; j++)
-				goal[i][j] = 3*i+j+1;
+		for(int i = 0; i < 8; i++)
+			goal[i] = i+1; 
 
-		goal[2][2] = 0;
+		goal[8] = 0;
 		goal_node.set_state(goal);
+	}
+	bool is_goal(Node n)
+	{
+		bool check = true;
+		int* a = n.get_state();
+		for(int i = 0; i < 9; i++)
+			if(goal[i] != a[i])
+				check = false;
+		return check;
 	}
 };
 
@@ -148,7 +145,7 @@ void * a_star_manhattan_distance_heuristic()
 //Overload the > operator.
 bool operator> (const  Node &node1, const Node &node2)
 {
-	return node1.priority < node2.priority;	
+	return node1.f_n < node2.f_n;	
 }
 
 
@@ -163,6 +160,7 @@ void general_search(Node problem, void *queuing_function())
 	//The priorities will be assigned in the Descending Order of priority
 	priority_queue<Node, vector<Node>,greater<vector<Node>::value_type> > nodes;
 	nodes.push(problem);
+	puzzle element;
 	while (1)
 	{
 		if(nodes.empty())
@@ -170,8 +168,8 @@ void general_search(Node problem, void *queuing_function())
 			cout<<"Is empty";
 			return;
 		} 
-        //puzzle element(nodes.top());
-        //nodes.pop();
+        element.set_current_node(nodes.top());
+        nodes.pop();
         //if problem.goal state() succeed then return node
         //node = queuing function()
 	}
@@ -192,7 +190,8 @@ void output(int total_nodes, int max_nodes, int depth)
 
 int main(int argc, char const *argv[])
 {
-	int puzzle_choice, algo_choice, start_state[3][3];;
+	int puzzle_choice, algo_choice;
+	int start_state[9] = {1,2,3,4,0,6,7,5,8};
 	
 	cout<<"Welcome to Abhishek's 8-puzzle solver."<<endl;
 	
@@ -202,20 +201,17 @@ int main(int argc, char const *argv[])
 	if(puzzle_choice == 1)
 	{
 		//generate intial state
-		int default_state[3][3] = {{1,2,3},{4,0,6},{7,5,8}};
 
 	} 
 	else if(puzzle_choice == 2)
 	{
 		cout<<"Enter your puzzle, use a zero to represent the blank"<<endl;
 		//puzzle input
-		for(int i = 0; i < 3; i++)
+		for(int i = 0; i < 9; i++)
 		{
-			cout<<"Enter row: "<<i+1<<"use space or tabs between numbers"<<endl;
-			for(int j = 0; j < 3; j++)
-			{
-				cin>>start_state[i][j];
-			}
+			if( (i+1)%3 == 2 )
+				cout<<"Enter row: "<<((i+1)/3)+1<<"use space or tabs between numbers"<<endl;
+			cin>>start_state[i];
 		}
 	}
 	else
